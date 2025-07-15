9c7658661b8a3b712914287c286f3f1a27a31d88
abhinayh56 <kumarabhinay53@gmail.com>
2025-07-15 20:14:49 +0530

    asdfadsf

---
+++ src/src_1/Data_store.h
@@ -0,0 +1,38 @@
+#ifndef DATA_STORE_H
+#define DATA_STORE_H
+
+#include <map>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stdint.h>
+#include <iostream>
+
+class Data_store
+{
+public:
+    Data_store(const Data_store &) = delete;
+    Data_store &operator=(const Data_store &) = delete;
+    static Data_store &getInstance();
+
+    template <typename T>
+    uint64_t register_element(std::string key_, std::string path_, T data_, std::size_t size_, bool overwrite_ = true);
+
+    template <typename T>
+    bool get(uint64_t index, T &data_, std::size_t size_);
+
+    template <typename T>
+    bool set(uint64_t index, const T &data_, std::size_t size_);
+
+private:
+    Data_store();
+    ~Data_store();
+
+    std::map<std::string, uint64_t> m_data_element_map; // key (string name of data element) : value (pointer of data element)
+    std::vector<uint8_t> m_data_buffer;
+    uint64_t m_offset = 0;
+};
+
+#include "Data_store.tpp"
+
+#endif // DATA_STORE_H
